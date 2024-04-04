bb04bd0c2d14a36f70cf9914753592b541aa5849
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-04 11:03:11 -0300

    Atv 241 - Curingas delimitados - Problema 2

--- US/application/Program241.java
+++ US/application/Program241.java
@@ -1,29 +1,40 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program241 {
 
+	/* Explicação
+		No geral, o código demonstra como genéricos com curingas (<? extends Number> e <? super Number>) podem ser usados para criar métodos flexíveis e reutilizáveis para trabalhar com listas contendo elementos de um tipo específico ou seus subtipos/supertipos.
+	*/
+
 	public static void main(String[] args) {
 
-		// Princípio get/put - contravariância
-		/*
-		 * Get - Error
-		 * put - Ok
-		 */
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
 
-		List<Object> myObjts = new ArrayList<Object>();
-		myObjts.add("Maria");
-		myObjts.add("Alex");
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+	}
 
-		// Lista pode ser um tipo ou qualquer supertipo de Number
-		List<? super Number> myNums = myObjts;
+	// Lista de Origem - List<? extends Number> --> covariancia (subtipos)
+	// Lista de Destino - List<? super Number> --> contravariancia (supertipos)
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		//Usa genéricos com curingas para aceitar listas de qualquer subtipo ou supertipo de Number.
+		for (Number number : source) {
+			destiny.add(number);
+		}
+	}
 
-		myNums.add(10);
-		myNums.add(3.14);
-
-		// Não podera acessar os objetos da lista, pois a lista pode ser um supertipo de Number
-		Number x = myNums.get(0); // erro de compilação
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj + " ");
+		}
+		System.out.println();
 	}
 }
