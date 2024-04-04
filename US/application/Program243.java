53d49d3be6112847ada8b987bc471afbf74d2ccb
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-04 14:13:48 -0300

    Atv 243 - Set - Demo 2

--- US/application/Program243.java
+++ US/application/Program243.java
@@ -1,32 +1,28 @@
 package application;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
-
-import entities.Product;
 
 public class Program243 {
     public static void main(String[] args) {
 
-        // Set<String> set = new HashSet<>();
-        // Set<String> set = new TreeSet<>();
-        Set<String> set = new LinkedHashSet<>();
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
 
-        set.add("TV");
-        set.add("Tablet");
-        set.add("Notebook");
+        // union
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
 
-        // set.remove("Tablet");
-
-        // set.removeIf(x -> x.length() >= 3);
-        // set.removeIf(x -> x.charAt(0) == 'T');
-
-        // System.out.println(set.contains("Notebook"));
-
-        for (String p : set) {
-            System.out.println(p);
-        }
+        // intersection
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println(d);
+        
+        // difference
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
     }
 }
