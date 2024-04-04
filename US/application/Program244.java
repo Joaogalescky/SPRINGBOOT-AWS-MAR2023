c47a84556f03a81ab4ed5a9f12a34d49d42452ea
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-04 14:22:36 -0300

    Atv 244 - Como Set testa igualdade - exp 1 - ponteiro

---
+++ US/application/Program244.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+import entities.Product;
+
+public class Program244 {
+    public static void main(String[] args) {
+
+        /*
+         * Explicacao
+         * A classe produtc não contem a implementacao do HashCode equals. Logo o Set
+         * vai utilizar a referencia de ponteiros para equiparar as variaveis
+         * Por serem instancias diferentes, a resposta vai ser "false"
+        */
+
+        Set<Product> set = new HashSet<>();
+
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        Product prod = new Product("Notebook", 1200.0);
+        System.out.println(set.contains(prod));
+    }
+}
