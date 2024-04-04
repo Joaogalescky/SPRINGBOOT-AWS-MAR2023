1a0d0877db66cb1e65fffef605605607610ee197
Joaogalescky <joaocvgalesky@gmail.com>
2024-04-04 19:57:26 -0300

    Atv 248 - Map - Demo 1

---
+++ US/application/Program248.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+//Aula 238 - Map<K,V> - Demo 1
+public class Program248 {
+    //kaySet() - retorna um Set<K>
+
+    public static void main(String[] args) {
+
+        Map<String, String> cookies = new TreeMap<>();
+
+        cookies.put("username", "Maria");
+        cookies.put("email", "Maria@gmail.com");
+        cookies.put("phone", "99711122");
+
+        cookies.remove("email");
+        cookies.put("phone", "99771133"); //vai substituir o valour da key "phone" - 997711122 por 99771133, pois há esta passando a mesma key com um valour diferente
+
+        System.out.println("==========================");
+        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+        System.out.println("Phone number: " + cookies.get("phone"));
+        System.out.println("Email: " + cookies.get("email"));
+        System.out.println("Size: " + cookies.size());
+
+        System.out.println("ALL COOKIES:");
+        for(String key : cookies.keySet()) {
+            System.out.println(key + ": " + cookies.get(key));
+        }
+    }
+}
