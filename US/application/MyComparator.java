93dc68d5d18f32c6d604300d8ad6f0d70f00bb93
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-05 09:30:59 -0300

    Atv 252 - Standard Comparator - Comentarios

--- US/application/MyComparator.java
+++ US/application/MyComparator.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 import entities.Product252;
 
 public class MyComparator implements Comparator<Product252> {
+    /* Comparator
+        Interface funcional do Java 8
+        A interface funcional é uma interface que tem apenas um método ABSTRATO, default ou estáticos.
+        Interface Comparator<T>
+    */
 
     @Override
     public int compare(Product252 p1, Product252 p2) {
