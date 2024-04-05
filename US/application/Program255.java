241cbf672b6f871b081f001b6f4cce146740cb79
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-05 14:01:28 -0300

    Atv 255 - Predicate - Declared lambda expression

--- US/application/Program255.java
+++ US/application/Program255.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import entities.Product255;
 import util.ProductPredicate;
+import java.util.function.Predicate;
 
 public class Program255 {
 
@@ -19,7 +20,12 @@ public class Program255 {
 		//list.removeIf( p -> p.getPrice() >= 100);
 		//list.removeIf(new ProductPredicate());
 		//list.removeIf(Product255::staticProductPredicate);
-		list.removeIf(Product255::nonStaticProductPredicate);
+		//list.removeIf(Product255::nonStaticProductPredicate);
+		double min = 100.0;
+
+		Predicate<Product255> pred = p -> p.getPrice() >= min;
+
+		list.removeIf(pred);
 
 		for (Product255 p : list) {
 			System.out.println(p);
