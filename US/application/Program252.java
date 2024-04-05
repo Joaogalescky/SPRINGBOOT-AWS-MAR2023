cd6e9466ef73c4652182ff0ad818659bcd4ee777
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-05 09:36:48 -0300

    Atv 252 - Anonymous class (Classe anônima)

--- US/application/Program252.java
+++ US/application/Program252.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import entities.Product252;
 
@@ -14,7 +15,20 @@ public class Program252 {
 		list.add(new Product252("Notebook", 1200.00));
 		list.add(new Product252("Tablet", 450.00));
 
-		list.sort(new MyComparator());
+		// Classe anônima
+		/*
+		 * Permitem criar um código mais conciso, por meio de declarações e
+		 * instanciações ao mesmo tempo em uma classe. São como classes locais,
+		 * desconsiderando que não possuem nome.
+		 */
+		Comparator<Product252> comp = new Comparator<Product252>() {
+			@Override
+			public int compare(Product252 p1, Product252 p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+
+		list.sort(comp);
 
 		for (Product252 p : list) {
 			System.out.println(p);
