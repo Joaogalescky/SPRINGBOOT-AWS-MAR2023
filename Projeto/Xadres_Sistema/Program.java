ccfafe1d4d0e1ff3a83efc21443e4ffa95e96e9c
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-06 16:15:31 -0300

    Seção 16 - 182 - Primeira classe = Position

--- Projeto/Xadres_Sistema/Program.java
+++
@@ -1,8 +0,0 @@
-package Projeto.Xadres_Sistema;
-
-public class Program {
-    public static void main(String[] args) {
-        
-        
-    }
-}
---
+++ Projeto/Xadres_Sistema/application/Program.java
@@ -0,0 +1,11 @@
+package Projeto.Xadres_Sistema.application;
+
+import Projeto.Xadres_Sistema.boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
