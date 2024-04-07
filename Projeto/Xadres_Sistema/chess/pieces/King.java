ea4cffa3b52da9e49c4b710149658a4dbb1892c6
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-07 18:09:47 -0300

    Seção 16 - 205 - Jogada especial Roque

--- Projeto/Xadres_Sistema/chess/pieces/King.java
+++ Projeto/Xadres_Sistema/chess/pieces/King.java
@@ -2,14 +2,18 @@ package Projeto.Xadres_Sistema.chess.pieces;
 
 import Projeto.Xadres_Sistema.boardgame.Board;
 import Projeto.Xadres_Sistema.boardgame.Position;
+import Projeto.Xadres_Sistema.chess.ChessMatch;
 import Projeto.Xadres_Sistema.chess.ChessPiece;
 import Projeto.Xadres_Sistema.chess.Color;
 
 // Rei
 public class King extends ChessPiece {
 
-    public King(Board board, Color color) {
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -20,6 +24,11 @@ public class King extends ChessPiece {
     private boolean canMove(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p == null || p.getColor() != getColor();
+    }
+
+    private boolean testRookCastling(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
 
     @Override
@@ -76,6 +85,28 @@ public class King extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
         }
 
+        // #Movimento especial - Roque/Castling
+        if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+            // #Movimento especial - Roque - Torre do lado do Rei
+            Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+            if (testRookCastling(posT1)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+            // #Movimento especial - Roque - Torre do lado da Rainha
+            Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+            if (testRookCastling(posT2)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+                Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+                    mat[position.getRow()][position.getColumn() - 2] = true;
+                }
+            }
+        }
         return mat;
     }
 }
