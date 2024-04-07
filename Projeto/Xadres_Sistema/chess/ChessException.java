04f7f434cb7b32e22d485d6711ee30c5e9ac67ba
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-07 12:31:51 -0300

    Seção 16 - 187 - ChessException e ChessPosition

---
+++ Projeto/Xadres_Sistema/chess/ChessException.java
@@ -0,0 +1,13 @@
+package Projeto.Xadres_Sistema.chess;
+
+public class ChessException extends RuntimeException{
+    
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+
+    
+
+}
