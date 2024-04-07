88cabc3832fc6ba58696c758832ac21d59389490
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-07 15:41:06 -0300

    Seção 16 - 196 - Manipulando peças capturadas

--- Projeto/Xadres_Sistema/chess/ChessMatch.java
+++ Projeto/Xadres_Sistema/chess/ChessMatch.java
@@ -1,5 +1,7 @@
 package Projeto.Xadres_Sistema.chess;
 
+import java.util.ArrayList;
+import java.util.List;
 import Projeto.Xadres_Sistema.boardgame.Board;
 import Projeto.Xadres_Sistema.boardgame.Piece;
 import Projeto.Xadres_Sistema.boardgame.Position;
@@ -11,6 +13,9 @@ public class ChessMatch {
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch() {
         board = new Board(8, 8);
@@ -58,6 +63,11 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+
+        if (capturedPiece != null) {
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
@@ -86,6 +96,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup() {
