717b49334abd18686142b741ae34e7228c0a22db
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-07 13:32:04 -0300

    Seção 16 - 190 - Tratando exceções e limpando a tela

--- Projeto/Xadres_Sistema/application/Program.java
+++ Projeto/Xadres_Sistema/application/Program.java
@@ -1,27 +1,39 @@
 package Projeto.Xadres_Sistema.application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import Projeto.Xadres_Sistema.chess.ChessException;
 import Projeto.Xadres_Sistema.chess.ChessMatch;
 import Projeto.Xadres_Sistema.chess.ChessPiece;
 import Projeto.Xadres_Sistema.chess.ChessPosition;
 
 public class Program {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		ChessMatch chessMatch = new ChessMatch();
-		
-		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-		}
-	}
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ChessMatch chessMatch = new ChessMatch();
+
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+        }
+    }
 }
