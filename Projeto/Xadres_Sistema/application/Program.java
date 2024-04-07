db2da0732a08b16e7e100506fb58accf9679e879
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-07 16:03:09 -0300

    Seção 16 - 198 - Lógica de xeque - PARTE 2

--- Projeto/Xadres_Sistema/application/Program.java
+++ Projeto/Xadres_Sistema/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -33,10 +33,10 @@ public class Program {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            
+
                 if (capturedPiece != null) {
-					captured.add(capturedPiece);
-				}
+                    captured.add(capturedPiece);
+                }
 
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
@@ -46,5 +46,7 @@ public class Program {
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
