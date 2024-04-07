a612b26b2e5df44619501a49dfb749b5c5bcdda6
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-07 12:14:18 -0300

    Seção 16 - 186 - BoardException e programação defensiva

---
+++ Projeto/Xadres_Sistema/boardgame/BoardException.java
@@ -0,0 +1,9 @@
+package Projeto.Xadres_Sistema.boardgame;
+
+public class BoardException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public BoardException(String msg) {
+        super(msg);
+    }
+}
