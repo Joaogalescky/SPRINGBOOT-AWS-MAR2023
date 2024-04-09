cbdf892e5fb0e5b71a0bdb406f13cc1bc6994b60
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-09 16:08:54 -0300

    Seção 20 - 257 -  Declared lambda expression

--- US/application/Program257.java
+++ US/application/Program257.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product257;
@@ -18,7 +19,9 @@ public class Program257 {
 		list.add(new Product257("Tablet", 350.50));
 		list.add(new Product257("HD Case", 80.90));
 
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 	}
